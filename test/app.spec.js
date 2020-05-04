cdd082698c56dafbba5df25d0052ea217565ce66
yud-cumba <yudith.cumba@gmail.com>
2020-05-04 11:39:52 -0500

    Configure el package.json con el bin

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,44 +5,44 @@ describe('Test de la función mdLinks', () => {
     expect(typeof mdLinks).toBe('function');
   });
 
-  it('deberia retornar un objeto de mdLinks ', () => {
+  it('deberia retornar un objeto de mdLinks con carpeta absoluta ', () => {
     mdLinks('/home/yudith/Documentos/Programacion/Proyectos/md-links/test/paraProbar', { validate: true })
       .then((link) => expect(typeof link).toBe('object'));
   });
 
-  it('Prueba con carpeta paraProbar y validate: false', () => {
-    mdLinks('/../../../../../../test/paraProbar', { validate: false })
+  it('Prueba con carpeta relativa paraProbar y validate: false', () => {
+    mdLinks('/home/yudith/Documentos/Programacion/Proyectos/md-links/test/paraProbar', { validate: false })
       .then((link) => expect(link).toEqual([
         {
           text: 'Wikipedia',
           href: 'https://es.wikipedia.org/wiki/Markdown',
-          file: '../../../../../../../../../README.md'
+          file: '../README.md',
         },
         {
           text: 'Node.js',
           href: 'https://nodejs.org/',
-          file: '../../../../../../../../../README.md'
+          file: '../README.md',
         },
         {
           text: 'Github Yud',
           href: 'https://github.com/yud.cumba/md-links',
-          file: '../../../../../../../../../README.md'
+          file: '../README.md',
         },
         {
           text: 'JW',
           href: 'https://www.jw.org',
-          file: '../../../../../../../../../../README2.md'
+          file: '../README2.md',
         },
         {
           text: 'JW',
           href: 'https://www.jw.org',
-          file: '../../../../../../../../../../README2.md'
+          file: '../README2.md',
         },
         {
           text: 'Hola',
           href: 'https://hola.xp',
-          file: '../../../../../../../../../../../README3.md'
-        }
+          file: '../README3.md',
+        },
       ]));
   });
 
@@ -52,45 +52,45 @@ describe('Test de la función mdLinks', () => {
         {
           text: 'Wikipedia',
           href: 'https://es.wikipedia.org/wiki/Markdown',
-          file: '../../../../../../../../../README.md',
+          file: '../README.md',
           status: 200,
-          status_text: 'ok'
+          status_text: 'ok',
         },
         {
           text: 'Node.js',
           href: 'https://nodejs.org/',
-          file: '../../../../../../../../../README.md',
+          file: '../README.md',
           status: 200,
-          status_text: 'ok'
+          status_text: 'ok',
         },
         {
           text: 'Github Yud',
           href: 'https://github.com/yud.cumba/md-links',
-          file: '../../../../../../../../../README.md',
+          file: '../README.md',
           status: 404,
-          status_text: 'fail'
+          status_text: 'fail',
         },
         {
           text: 'JW',
           href: 'https://www.jw.org',
-          file: '../../../../../../../../../../README2.md',
+          file: '../README2.md',
           status: 200,
-          status_text: 'ok'
+          status_text: 'ok',
         },
         {
           text: 'JW',
           href: 'https://www.jw.org',
-          file: '../../../../../../../../../../README2.md',
+          file: '../README2.md',
           status: 200,
-          status_text: 'ok'
+          status_text: 'ok',
         },
         {
           text: 'Hola',
           href: 'https://hola.xp',
-          file: '../../../../../../../../../../../README3.md',
+          file: '../README3.md',
           status: '',
           status_text: 'Not found',
-        }
+        },
       ]));
   });
 });
