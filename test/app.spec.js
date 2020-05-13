b2cb26320bcc54ba2e26a7d6d69cd6f68b1608a9
yud-cumba <yudith.cumba@gmail.com>
2020-05-13 10:44:50 -0500

    mock failed

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,23 +1,9 @@
+//const jest = require('jest');
+const functions = require('../lib/__mocks__/app_functions.mock');
+
 const { mdLinks } = require('../lib/app.js');
 
 jest.setTimeout(50000);
-
-const validateOff = [
-  { text: 'Hola', href: 'https://hola.xp', file: 'README3.md' },
-  { text: 'JW', href: 'https://www.jw.org', file: 'README2.md' },
-  { text: 'JW', href: 'https://www.jw.org', file: 'README2.md' },
-  {
-    text: 'Wikipedia',
-    href: 'https://es.wikipedia.org/wiki/Markdown',
-    file: 'README.md',
-  },
-  { text: 'Node.js', href: 'https://nodejs.org/', file: 'README.md' },
-  {
-    text: 'Github Yud',
-    href: 'https://github.com/yud.cumba/md-links',
-    file: 'README.md',
-  },
-];
 
 const validateOn = [
   {
@@ -64,32 +50,32 @@ const validateOn = [
   },
 ];
 
-describe('Test de la función mdLinks con carpeta paraProbar ', () => {
-  it('deberia ser una funcion', () => {
-    expect(typeof mdLinks).toBe('function');
+jest.describe('Test de la función mdLinks con carpeta paraProbar ', () => {
+  jest.it('deberia ser una funcion', () => {
+    jest.expect(typeof mdLinks).toBe('function');
   });
 
-  it('deberia retornar un objeto de mdLinks con carpeta absoluta ', () => mdLinks('./test/paraProbar', { validate: true })
-    .then((link) => expect(typeof link).toBe('object')));
+  jest.it('deberia retornar un objeto de mdLinks con carpeta absoluta ', () => mdLinks('./test/paraProbar', { validate: true })
+    .then((link) => jest.expect(typeof link).toBe('object')));
 
-  it('Prueba con carpeta relativa paraProbar y validate: false', () => mdLinks('./test/paraProbar', { validate: false })
-    .then((link) => expect(link).toEqual(validateOff)));
+  jest.it('Prueba con carpeta relativa paraProbar y validate: false', () => mdLinks('./test/paraProbar', { validate: false })
+    .then((link) => jest.expect(link).toEqual(validateOff)));
 
-  it('Prueba con carpeta paraProbar y validate: true', () => mdLinks('./test/paraProbar', { validate: true })
-    .then((link) => expect(link).toStrictEqual(validateOn)));
+  jest.it('Prueba con carpeta paraProbar y validate: true', () => mdLinks('./test/paraProbar', { validate: true })
+    .then((link) => jest.expect(link).toStrictEqual(validateOn)));
 
-  it('mdLinks("./test/paraProbr") sin segundo parámetro', () => mdLinks('./test/paraProbar')
-    .then((link) => expect(link).toEqual(validateOff)));
+  jest.it('mdLinks("./test/paraProbr") sin segundo parámetro', () => mdLinks('./test/paraProbar')
+    .then((link) => jest.expect(link).toEqual(validateOff)));
 
-  it('mdLinks("./test/paraProbar/cipher.js") sin segundo parámetro', () => mdLinks('./test/paraProbar/cipher.js')
-    .then((link) => expect(link.length).toEqual(0)));
+  jest.it('mdLinks("./test/paraProbar/cipher.js") sin segundo parámetro', () => mdLinks('./test/paraProbar/cipher.js')
+    .then((link) => jest.expect(link.length).toEqual(0)));
 
-  it('mdLinks("./test/paraProbarVacio") sin segundo parámetro', () => mdLinks('./test/paraProbarVacio')
-    .then((link) => expect(link.length).toEqual(0)));
+  jest.it('mdLinks("./test/paraProbarVacio") sin segundo parámetro', () => mdLinks('./test/paraProbarVacio')
+    .then((link) => jest.expect(link.length).toEqual(0)));
 
-  it('Prueba con carpeta inválida', () => mdLinks('./test/paraProbr', { validate: false })
-    .catch((err) => expect(err).toMatch('./test/paraProbr is not a valid path, please try again')));
+  jest.it('Prueba con carpeta inválida', () => mdLinks('./test/paraProbr', { validate: false })
+    .catch((err) => jest.expect(err).toMatch('./test/paraProbr is not a valid path, please try again')));
 
-  it('mdLinks("./test/paraProbar", true) con segundo parámetro inválido', () => mdLinks('./test/paraProbar', true)
-    .catch((err) => expect(err).toMatch('Parámetro inválido')));
+  jest.it('mdLinks("./test/paraProbar", true) con segundo parámetro inválido', () => mdLinks('./test/paraProbar', true)
+    .catch((err) => jest.expect(err).toMatch('Parámetro inválido')));
 });
